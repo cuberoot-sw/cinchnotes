23fc804aa67fa04b38014c1637cf06defec44aaa
prachidhabu <prachi@cuberoot.in>
2013-08-30 15:03:02 +0530

    added associations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :user
+  belongs_to :category
   attr_accessible :description, :due_date, :note, :status
 
   # state_machine
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   has_many :contacts
   has_many :events
   has_many :tasks
+  has_many :categories
 
   acts_as_tagger
 
