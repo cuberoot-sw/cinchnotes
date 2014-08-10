d28d23277b4f0a335a7a5c6f66548cd36976a844
Girish S <girish.sonawane@gmail.com>
2014-08-10 12:46:30 +0530

    increased width

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :use_sudo, false
 
 ssh_options[:forward_agent] = true
 set :scm, :git
-set :repository, "git@github.com:cuberoot-sw/cinch-notes.git"
+set :repository, "https://github.com/cuberoot-sw/cinchnotes.git"
 set :branch, 'master'
 set :git_shallow_clone, 1
 
@@ -19,14 +19,14 @@ role :db, domain, :primary => true
 
 set :deploy_via, :remote_cache
 
-desc "Symlink the database config file from shared directory to current release directory." 
+desc "Symlink the database config file from shared directory to current release directory."
 task :symlink_database_yml do
   run "cd #{release_path} && ln -nsf #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  run "ln -nsf #{shared_path}/production.rb #{release_path}/config/environments/production.rb" 
-end 
+  run "ln -nsf #{shared_path}/production.rb #{release_path}/config/environments/production.rb"
+end
 
 after :deploy, 'symlink_database_yml'
- 
+
 namespace :deploy do
   task :restart do
     # Hide default restart task
